9368cb9bd0539ba04c96cb23cda7b9c49895d505
agung <agungsuprayitno>
2018-09-12 19:45:30 +0700

    Edit product variant.

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,15 @@
 import UserApi from './module/UserApi'
 import Access from './module/AccessApi'
 import Product from './module/ProductApi'
+import ProductVariant from './module/ProductVariantApi'
 
 export const AccessApi = Access
 export const ProductApi = Product
+export const ProductVariantApi = ProductVariant
 
 export default {
   UserApi,
   AccessApi,
-  ProductApi
+  ProductApi,
+  ProductVariantApi
 }
