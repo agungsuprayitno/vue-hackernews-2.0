5784c941a4a3c38723d70797f498cc4e62124e85
Kendy <kendychow@gmail.com>
2018-11-22 12:08:15 +0700

    revert httpmodule

--- src/module/HttpModule.js
+++ src/module/HttpModule.js
@@ -4,27 +4,22 @@ import { isEmpty } from 'lodash'
 import {readCookie, key} from '@/module/CacheModule'
 
 const token = function () {
-  // const token = readCookie(key)
+  const token = readCookie(key)
 
-  // if (isEmpty(token) || (!isEmpty(token) && moment().diff(moment(token.expired)) > 0)) {
-  //   // return null
-  // }
+  if (isEmpty(token) || (!isEmpty(token) && moment().diff(moment(token.expired)) > 0)) {
+    return null
+  }
 
-  //return token.token
-  return 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhbmRyZWFzMkBnbWFpbC5jb20iLCJqd3RJRCI6Ikp1SkEwMUo2S2dleUtFeUdEWnZNNXlhT1dLZHlxSWtkIiwidXNlcm5hbWUiOiJhbmRyZWFzMkBnbWFpbC5jb20iLCJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjkwOTAvdjEvYXV0aCIsImlhdCI6MTU0MjI3MDI0MDMwMSwiZXhwIjoxNTQyMjczODQwMzAxfQ.pvnI-U-DweyB4786iqX-QmfjJZQYH7xejGSLEp9XmjjQlL9A_kYcLiaeBu6V9FmIJli6GPUPyTDRd-wefnYoMw'
+  return token.token
 }
 export const VoucherGeneratorAxios = axios.create({
   baseURL: process.env.VOUCHER_GENERATOR_API_URL,
   headers: {
-    'Authorization': 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhbmRyZWFzMkBnbWFpbC5jb20iLCJqd3RJRCI6Ikp1SkEwMUo2S2dleUtFeUdEWnZNNXlhT1dLZHlxSWtkIiwidXNlcm5hbWUiOiJhbmRyZWFzMkBnbWFpbC5jb20iLCJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjkwOTAvdjEvYXV0aCIsImlhdCI6MTU0MjI3MDI0MDMwMSwiZXhwIjoxNTQyMjczODQwMzAxfQ.pvnI-U-DweyB4786iqX-QmfjJZQYH7xejGSLEp9XmjjQlL9A_kYcLiaeBu6V9FmIJli6GPUPyTDRd-wefnYoMw',
-    'Access-Control-Allow-Origin': '*'
-}
-  // headers: {
-  //   Authorization: token() ? 'Bearer ' + token() : null,
-  //   'X-Requested-With': 'XMLHttpRequest',
-  //   'Access-Control-Allow-Origin': '*',
-  //   'Accept': 'application/json'
-  // }
+    Authorization: token() ? 'Bearer ' + token() : null,
+    'X-Requested-With': 'XMLHttpRequest',
+    'Access-Control-Allow-Origin': '*',
+    'Accept': 'application/json'
+  }
 })
 
 VoucherGeneratorAxios.interceptors.response.use(function (response) {
