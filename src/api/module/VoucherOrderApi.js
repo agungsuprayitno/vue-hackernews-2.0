087953c3d79616c8c54099801fdc3198ab5a3fc7
agung <agungsuprayitno>
2018-11-29 14:10:18 +0700

    Add multiple sku code.

--- src/api/module/VoucherOrderApi.js
+++ src/api/module/VoucherOrderApi.js
@@ -4,7 +4,6 @@ export default {
 
   async getVoucherOrderApi(pagination) {
     let queryParams = setPagination(pagination)
-    //VoucherGeneratorAxios.headers('Authorization','Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhbmRyZWFzMkBnbWFpbC5jb20iLCJqd3RJRCI6Ikp1SkEwMUo2S2dleUtFeUdEWnZNNXlhT1dLZHlxSWtkIiwidXNlcm5hbWUiOiJhbmRyZWFzMkBnbWFpbC5jb20iLCJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjkwOTAvdjEvYXV0aCIsImlhdCI6MTU0MjI3MDI0MDMwMSwiZXhwIjoxNTQyMjczODQwMzAxfQ.pvnI-U-DweyB4786iqX-QmfjJZQYH7xejGSLEp9XmjjQlL9A_kYcLiaeBu6V9FmIJli6GPUPyTDRd-wefnYoMw');
     return await VoucherGeneratorAxios.get("/v1/rest/order" + queryParams).then(async (voucherOrders) => {
       return await voucherOrders.data
     }).catch((error) => {
@@ -19,20 +18,4 @@ export default {
       throw error
     })
   },
-
-  async createMerchantApi(merchant) {
-    return await VoucherGeneratorAxios.post("/v1/merchant", merchant).then(async (merchant) => {
-      return await merchant.data
-    }).catch((error) => {
-      throw error
-    })
-  },
-
-  async updateMerchantApi(merchant) {
-    return await VoucherGeneratorAxios.put("/v1/merchant/" + merchant.id, merchant).then(async (merchant) => {
-      return await merchant.data
-    }).catch((error) => {
-      throw error
-    })
-  }
 }
--- src/views/voucher-order/VoucherOrderForm.vue
+++ src/views/voucher-order/VoucherOrderForm.vue
@@ -12,24 +12,37 @@
             <span v-show="errors.has('remark')" class="text-danger is-danger">{{ errors.first('remark') }}</span>
           </b-form-group>
 
+          <b-form-group label="Active status" :label-cols="3" :horizontal="true">
+            <b-form-select size="md" :options="activeStatus" value-field="id" text-field="title" v-model="voucherInput.active" v-validate="'required'" data-vv-as="Active status" name="active" value="Please select" class="mb3">
+            </b-form-select>
+            <div class="col-12 my-2">If active status set to "Yes", the voucher will be generated with the secret code</div>
+          </b-form-group>
+
           <b-form-group label="" :label-cols="3" :horizontal="true">
-            <div class="row">
+            <div class="row" v-for="(voucherOrderItem, index) in voucherOrderItemArr" :key="index">
+
               <div class="col-md-4">
                 <label>Product</label>
-                <b-form-select size="md" :options="productList" value-field="id" text-field="title" v-model="voucherInput.product" v-validate="'required'" data-vv-as="Product" name="product" value="Please select" class="mb3" v-on:input="productOnChange()">
+                <b-form-select size="md" :options="productList" value-field="id" text-field="title" v-model="voucherOrderItem.product" v-validate="'required'" data-vv-as="Product" name="product" value="Please select" class="mb3" v-on:input="productOnChange(voucherOrderItem.product)">
                 </b-form-select>
                 <span v-show="errors.has('product')" class="text-danger is-danger">{{ errors.first('product') }}</span>
-                <!--b-form-input v-model="voucherInput.productVariant" data-vv-as="Product Variant" name="product_variant" type="text"></b-form-input-->
               </div>
               <div class="col-md-4">
                   <label>Product Variant</label>
-                <b-form-select size="md" :options="productVariantList" value-field="id" text-field="title" v-model="voucherInput.productVariant" v-validate="'required'" data-vv-as="Product variant" name="product_variant" value="Please select" class="mb3">
+                <b-form-select size="md" :options="productVariantList" value-field="id" text-field="title" v-model="voucherOrderItem.productVariant" v-validate="'required'" data-vv-as="Product variant" name="product_variant" value="Please select" class="mb3">
                 </b-form-select>
-                <!--b-form-input v-model="voucherInput.productVariant" data-vv-as="Product Variant" name="product_variant" type="text"></b-form-input-->
+                <span v-show="errors.has('product_variant')" class="text-danger is-danger">{{ errors.first('product_variant') }}</span>
               </div>
-              <div class="col-md-4">
+              <div class="col-md-2">
                   <label>Quantity</label>
-                  <b-form-input v-model="voucherInput.quantity" data-vv-as="Quantity" name="quantity" type="text" ></b-form-input>
+                  <b-form-input v-model="voucherOrderItem.quantity" data-vv-as="Quantity" name="quantity" type="text" v-validate="'required|numeric'"></b-form-input>
+                <span v-show="errors.has('quantity')" class="text-danger is-danger">{{ errors.first('quantity') }}</span>
+              </div>
+              <div class="col-md-1 row align-items-end">
+                <button class="btn btn-link" @click="addVoucherOrderItemArr"><i class="fas fa-plus-square fa-2x"></i> </button>
+              </div>
+              <div class="col-md-1 row align-items-end">
+                <button class="btn btn-link" @click="removeVoucherOrderItemArr(index)"><i class="fas fa-trash fa-2x"></i> </button>
               </div>
             </div>
           </b-form-group>
@@ -45,21 +58,22 @@
 </template>
 <script>
 
-import {mapState, mapActions, mapGetters} from 'vuex'
+import {mapState, mapActions} from 'vuex'
 import { mapWaitingActions } from 'vue-wait'
 export default {
   data () {
     return {
-      options: [
-        { id: this.$constant.status.activeStatus, title: this.$constant.status.activeStatus },
-        { id: this.$constant.status.inactiveStatus, title: this.$constant.status.inactiveStatus }
-      ],
       resetValues: {},
       voucherInput: {
         remark: '',
-        quantity: '15',
-        active: 'NO'
-      }
+        active: ''
+      },
+      activeStatus: [
+        { id: 'NO', title: 'No' },
+        { id: 'YES', title: 'Yes' }
+      ],
+      voucherOrderItemArr: [],
+      blockRemoval: true,
     }
   },
 
@@ -70,13 +84,18 @@ export default {
     })
   },
 
-  asyncData ({store, route}) {
-    store.dispatch('Product/getAllProductList')
-  },
-
   mounted () {
     this.resetValues = {
       // voucher: JSON.parse(JSON.stringify(this.voucher))
+    }
+    //  create new row product variant and quantity
+    this.addVoucherOrderItemArr()
+    this.$store.dispatch('Product/getAllProductList')
+  },
+
+  watch: {
+    voucherOrderItemArr () {
+      this.blockRemoval = this.voucherOrderItemArr.length <= 1
     }
   },
 
@@ -91,42 +110,64 @@ export default {
       createVoucher: 'generating voucher',
       getVoucherOrderById: 'getting voucher order',
     }),
-    productOnChange() {
-      this.getProductVariantListByProductId(this.voucherInput.product)
+    productOnChange(productId) {
+      this.getProductVariantListByProductId(productId)
+    },
+    addVoucherOrderItemArr () {
+      let checkEmptyVoucherOrderItemArr = this.voucherOrderItemArr.filter(voucherOrderItem => voucherOrderItem.number === null)
+
+      if (checkEmptyVoucherOrderItemArr.length >= 1 && this.voucherOrderItemArr.length > 0) return
+
+      this.voucherOrderItemArr.push({
+        product: this.productList,
+        productVariant: this.productVariantList,
+        quantity: 15
+      })
+    },
+    removeVoucherOrderItemArr (voucherOrderItemId) {
+      if (!this.blockRemoval) this.voucherOrderItemArr.splice(voucherOrderItemId, 1)
     },
     submit() {
       let __self = this
 
-      let voucherOrderItemArr = [];
-      if(__self.voucherInput.productVariant && __self.voucherInput.quantity) {
-        if(confirm('Are you sure want to generate this voucher(s)?')) {
-          voucherOrderItemArr.push({ 
-            "productVariantId": __self.voucherInput.productVariant,
-            "quantity": __self.voucherInput.quantity
-          });
-         
+      if(confirm('Are you sure want to generate this voucher(s)?')) {
+
+        // mapping key and value voucherOrderItemArr to be voucherOrderItem
+        let voucherOrderItem = __self.voucherOrderItemArr.flatMap(function (order) {
+          let orderItem = {
+            "productVariantId": parseInt(order.productVariant),
+            "quantity": parseInt(order.quantity)
+          };
+
+          return (order.quantity == 0 || __self.$lodash.isEmpty(order.product) || isNaN(order.product) || isNaN(order.productVariant)) ? null : orderItem;
+        })
+
+        // validate 'null' contain voucherOrderItem
+        let isNotValidVariant = voucherOrderItem.some(item => {
+          return item == null
+        } );
+
+        if(!isNotValidVariant) {
           let input = {
             remark: __self.voucherInput.remark,
-            voucherOrderItem: voucherOrderItemArr,
-            active: 'NO'
+            voucherOrderItem: voucherOrderItem,
+            active: __self.voucherInput.active
           }
-
-            //  dispatch Actions Create on Voucher
+          //  dispatch Actions Create on Voucher
           this.$validator.validateAll().then((result) => {
             if (result) {
-              if(!this.$lodash.isEmpty(this.$route.params.voucherOrderId)){
-                  input.id = __self.$route.params.voucherOrderId
-                  __self.updateVoucher(input)
-              }else {
-                __self.createVoucher({voucherOrder: input, router: __self.$router})
-              }
+              __self.createVoucher({voucherOrder: input, router: __self.$router})
             } else {
               if (process.env.VUE_ENV !== 'server') {
                 window.scrollTo(0, 0)
               }
             }
           })
+        } else {
+          //  TODO: jadi error form or toast
+          alert("Product, Product Variant or Quantity cannot be blank or 0")
         }
+
       }
     },
     reset() {
