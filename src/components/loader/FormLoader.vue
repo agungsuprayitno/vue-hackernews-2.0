70fad3ec66419cddd23ca30a517a22a28b0f7b36
agung <agungsuprayitno>
2018-12-06 14:35:07 +0700

    Add loader on login.

---
+++ src/components/loader/FormLoader.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <b-progress :value="100" variant="primary" :striped="true" :animated="true" class="mb-2"></b-progress>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "FormLoader"
+  }
+</script>
+
+<style scoped>
+
+</style>
